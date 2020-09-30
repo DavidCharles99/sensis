002f365dd13bf0c3a9ca89505bf2dac615d3752b
David <charlesinvestingltd@gmail.com>
2020-09-30 14:24:37 -0400

    added some things dont worry about it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def home
     render :plain => "hello world"
   end
+
+  def index
+    @posts = Post.all
+    render 'posts/index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'pages#home'
   get '/hello_world' => 'posts#home'
 
+  get '/posts' => 'posts#index'
+  get '/about' => 'static#about'
+  get '/team' => 'static#team'
   get 'pages/home'
-  get 'pages/about'
   get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
